5bc4896096d0356cdfd41232807df7178b13d171
MoisesSanchez2020 <san19039@byui.edu>
2020-12-03 15:02:52 -0800

    make it better

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:   "53b041f63e3c333dbe3d192222ec7210"
+    }
+}
+module.exports = constants;
